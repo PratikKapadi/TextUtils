ca8da51e65f76d78ec4f02f6440ea1e869b2e260
PratikKapadi <pratikkapdi13@gmail.com>
2024-08-24 20:19:34 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import About from './component/About';
 import Navbar from './component/Navbar';
 import TextFrom from './component/TextFrom';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar title="TextUtils" />
       <TextFrom heading="Enter the txet to analyze below"/>
+      <About/>
     </div>
   );
 }
